eb3b87f5f6ef7b2553da56030256830237cb35b2
Jason Nutt <jnutt367@gmail.com>
2021-04-07 15:15:07 -0400

    commit newest animation

--- gsap.js
+++ gsap.js
@@ -4,7 +4,10 @@ gsap.to(".box", {
         { duration: 1, backgroundColor: "#f38630", color: "red", delay: 0.5 },
         { duration: 1, x: 0, rotation: -360, scale: 2.5, color: "purple", delay: 0.5 },
         { duration: 2, y: 100, backgroundColor: "black", color: "aqua" },
-        { duration: 1, x: 300, y: 300 }
+        { duration: 1, x: 300, y: 300 },
+        { duration: 4, y: 600, backgroundColor: "blue", color: "pink" },
+        { duration: 1, x: 200, y: 100, scale: 4, backgroundColor: "black", border: "2px solid red", color: "green" }
+
     ]
 });
 
