73ee264d71268d288c5655eb293113b3ee123c88
Jason Nutt <jnutt367@gmail.com>
2021-04-07 14:50:20 -0400

    commit for gsap7 start draft

---
+++ gsap.js
@@ -0,0 +1,22 @@
+gsap.to(".box", {
+    keyframes: [
+        { duration: 1, x: 400, y: 600, color: "teal" },
+        { duration: 1, backgroundColor: "#f38630", color: "red", delay: 0.5 },
+        { duration: 1, x: 0, rotation: -360, scale: 2.5, color: "purple", delay: 0.5 },
+        { duration: 2, y: 100, backgroundColor: "black", color: "aqua" },
+        { duration: 1, x: 300, y: 300 }
+    ]
+});
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Whatup!!?");
