89359ad8ffada082c8f8c00702556ecbcdc5f849
Jason Nutt <jnutt367@gmail.com>
2021-04-07 15:23:40 -0400

    commit with 7 timelines

--- gsap.js
+++ gsap.js
@@ -1,12 +1,12 @@
 gsap.to(".box", {
     keyframes: [
-        { duration: 1, x: 400, y: 600, color: "teal" },
+        { duration: 1, x: 400, y: 600, border: "3px inset silver", color: "teal" },
         { duration: 1, backgroundColor: "#f38630", color: "red", delay: 0.5 },
         { duration: 1, x: 0, rotation: -360, scale: 2.5, color: "purple", delay: 0.5 },
         { duration: 2, y: 100, backgroundColor: "black", color: "aqua" },
-        { duration: 1, x: 300, y: 300 },
+        { duration: 1, x: 300, y: 300, border: "4px dotted black" },
         { duration: 4, y: 600, backgroundColor: "blue", color: "pink" },
-        { duration: 1, x: 200, y: 100, scale: 4, backgroundColor: "black", border: "2px solid red", color: "green" }
+        { duration: 1, x: 200, y: 100, rotation: 360, scale: 4, backgroundColor: "aqua", border: "3px inset red", color: "green" }
 
     ]
 });
