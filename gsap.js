738d0882b9ff55f5ff02606f99bafaa41d1da2ad
Jason Nutt <jnutt367@gmail.com>
2021-04-07 16:55:33 -0400

    commit for final draft before designer consultation

--- gsap.js
+++ gsap.js
@@ -6,7 +6,8 @@ gsap.to(".box", {
         { duration: 2, y: 100, backgroundColor: "black", color: "aqua" },
         { duration: 1, x: 300, y: 300, border: "4px dotted black" },
         { duration: 4, y: 600, backgroundColor: "blue", color: "pink" },
-        { duration: 1, x: 200, y: 100, rotation: 360, scale: 4, backgroundColor: "aqua", border: "3px inset red", color: "green" }
+        { duration: 1, x: 200, y: 100, rotation: 360, scale: 4, backgroundColor: "aqua", border: "3px inset red", color: "green" },
+        { duration: 9, x: 100, y: 100, rotation: -360, scale: 1, backgroundColor: "red", border: "3px inset red", color: "aqua" }
 
     ]
 });
@@ -24,7 +25,7 @@ gsap.to(".object", {
 gsap.to(".object2", {
     keyframes: [
         { duration: 1, x: 200, y: 300, border: "3px inset silver", color: "teal" },
-        { duration: 1, backgroundColor: "#f38630", color: "red", delay: 0.5 },
+        { duration: 1, backgroundColor: "grey", color: "red", delay: 0.5 },
         { duration: 1, x: 200, y: 200, borderRadius: "50%", textAlign: "center", backgroundColor: "#f38630", color: "green", delay: 0.9 },
         { duration: 3, x: 100, y: 100, display: "grid", alignItems: "center", justifyContent: "center", backgroundColor: "red", height: 200, width: 200, color: "blue", delay: 0.9 }
 
